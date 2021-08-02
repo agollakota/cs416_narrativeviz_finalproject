933f2d4a6945ef51597f48321a0db48eccd5cdf8
Aparna Gollakota <AparnaGollakota@Aparnas-MacBook-Air.local>
2021-08-01 23:45:04 -0500

    final

--- chart3.js
+++ chart3.js
@@ -16,11 +16,9 @@ d3.csv("https://raw.githubusercontent.com/agollakota/cs416_narrativeviz_finalpro
 }, function(error, dataset) {
   if (error) throw error;
 
-  // set domains for x and y scales
   xScale.domain(dataset.map(function(d) { return d.CRIME; }));
   yScale.domain([0, d3.max(dataset, function(d) { return d.COUNT; })]);
 
-  // add chart title
   g.append("text")
         .attr("x", (w / 2))             
         .attr("y", 0 - (margin.top / 2))
@@ -28,15 +26,13 @@ d3.csv("https://raw.githubusercontent.com/agollakota/cs416_narrativeviz_finalpro
         .attr("font-family", "sans-serif")
         .attr("font-weight", "bold")
         .style("font-size", "18px") 
-        .text("Crime in Chicago 2020");
+        .text("Crime in Chicago 2021");
 
-  // x axis
   g.append("g")
       .attr("class", "axis x-axis")
       .attr("transform", "translate(0," + h + ")")
       .call(d3.axisBottom(xScale));
 
-  // y axis
   g.append("g")
       .attr("class", "axis y-axis")
       .call(d3.axisLeft(yScale))
@@ -44,7 +40,6 @@ d3.csv("https://raw.githubusercontent.com/agollakota/cs416_narrativeviz_finalpro
       .attr("transform", "rotate(-90)")
       .attr("text-anchor", "end");
 
-  // create bars
   g.selectAll(".bar")
     .data(dataset)
     .enter().append("rect")
@@ -55,18 +50,14 @@ d3.csv("https://raw.githubusercontent.com/agollakota/cs416_narrativeviz_finalpro
       .attr("height", function(d) { return h - yScale(d.COUNT); })
       .attr("fill", "purple")
 
-      // On mouseover add a different fill as well as labels
       .on("mouseover", function(d) {
         d3.select(this)
           .attr("fill", "green");
 
-          //Get this bar's x/y values, then augment for the tooltip
           var xPosition = parseFloat(d3.select(this).attr('x')) + xScale.bandwidth() / 2;
           var yPosition = parseFloat(d3.select(this).attr("y")) - 5;
 
-          //Create the tooltip label
           g.append("text")
-             // .data(dataset)
              .attr("id", "tooltip")
              .attr("x", xPosition)
              .attr("y", yPosition)
@@ -78,7 +69,6 @@ d3.csv("https://raw.githubusercontent.com/agollakota/cs416_narrativeviz_finalpro
              .text(d.COUNT);
       })
 
-      // set back to normal when no longer mousing over bar
       .on("mouseout", function(d) {
         d3.select("#tooltip").remove();
         d3.select(this)
@@ -87,7 +77,6 @@ d3.csv("https://raw.githubusercontent.com/agollakota/cs416_narrativeviz_finalpro
           .attr("fill", "purple");
       });
 
-  // rotate  x-axis text for readability
   d3.selectAll(".x-axis .tick text")
     .attr("transform", "rotate(60) translate(8, -10)")
     .attr("font-family", "sans-serif")    
